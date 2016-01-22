42710d7f7022e4b3c4399135603c9930b290937c
daoanhvu <daoanhvu122@gmail.com>
2016-01-23 04:35:31 +0700

    Start writting UI

---
+++ java/NautilusNet/src/nautilus/ai/ui/ImageForm.java
@@ -0,0 +1,18 @@
+package nautilus.ai.ui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ImageForm extends JFrame {
+	
+	private JPanel mCharImagePane;
+	private JButton mStartLearning;
+	private JButton mRecognize;
+	
+	public ImageForm() {
+		
+	}
+	
+	
+}
