3e0b88de93aca15975f15ffe2a0d5056a9d06e39
daoanhvu <daoanhvu122@gmail.com>
2016-03-03 07:39:24 +0700

    writing android app

--- android/WritingPane/app/src/main/java/nautilus/writingpane/MainActivity.java
+++ android/WritingPane/app/src/main/java/nautilus/writingpane/MainActivity.java
@@ -2,12 +2,19 @@ package nautilus.writingpane;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WritingPane writingPane;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        writingPane = (WritingPane)findViewById(R.id.writingPane);
+        tvResult = (TextView)findViewById(R.id.tvResult);
     }
 }
--- android/WritingPane/app/src/main/java/nautilus/writingpane/MyApplication.java
+++ android/WritingPane/app/src/main/java/nautilus/writingpane/MyApplication.java
@@ -2,6 +2,10 @@ package nautilus.writingpane;
 
 import android.app.Application;
 
+import java.io.File;
+
+import nautilus.ai.model.NautilusNet;
+
 /**
  * Created by davu on 3/1/2016.
  */
@@ -9,10 +13,25 @@ public class MyApplication extends Application {
 
     public static MyApplication instance;
 
+    //
+    private NautilusNet mNetwork;
+
     @Override
     public void onCreate(){
         super.onCreate();
         instance = this;
+
+        initANN();
     }
+
+    private void initANN() {
+        File dir = getFilesDir();
+
+
+    }
+	
+		public NautilusNet getANN() {
+		return mTheNet;
+	}
 
 }
